f80ce1d911b5c3af46f4b0b19566ea6ddf3886aa
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2022-05-14 13:10:25 +0200

    Renamed the classes with uppercase

--- Cricket/app/src/main/java/com/ehb/cricket/Country.kt
+++ Cricket/app/src/main/java/com/ehb/cricket/Country.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [player.newInstance] factory method to
+ * Use the [country.newInstance] factory method to
  * create an instance of this fragment.
  */
-class player : Fragment() {
+class Country : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class player : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_player, container, false)
+        return inflater.inflate(R.layout.fragment_country, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class player : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment player.
+         * @return A new instance of fragment country.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            player().apply {
+            Country().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- Cricket/app/src/main/java/com/ehb/cricket/Match.kt
+++ Cricket/app/src/main/java/com/ehb/cricket/Match.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [country.newInstance] factory method to
+ * Use the [match.newInstance] factory method to
  * create an instance of this fragment.
  */
-class country : Fragment() {
+class Match : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class country : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_country, container, false)
+        return inflater.inflate(R.layout.fragment_match, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class country : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment country.
+         * @return A new instance of fragment match.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            country().apply {
+            Match().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
