141a977ada795e6dd26ddb93781c0bab348e2b95
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2022-05-14 13:11:15 +0200

    Show different content in each tab

--- Cricket/app/src/main/java/com/ehb/cricket/ui/main/SectionsPagerAdapter.kt
+++ Cricket/app/src/main/java/com/ehb/cricket/ui/main/SectionsPagerAdapter.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.ehb.cricket.Country
+import com.ehb.cricket.Match
+import com.ehb.cricket.Player
 import com.ehb.cricket.R
 
 private val TAB_TITLES = arrayOf(
@@ -22,7 +25,16 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     override fun getItem(position: Int): Fragment {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1)
+
+        when(position){
+            0 -> return Match()
+            1 -> return Player()
+            2 -> return Country()
+
+            else -> {
+                return PlaceholderFragment.newInstance(position + 1)
+            }
+        }
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
@@ -31,6 +43,6 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
 
     override fun getCount(): Int {
         // Show amount total pages (tabs).
-        return 3
+        return TAB_TITLES.size
     }
 }
