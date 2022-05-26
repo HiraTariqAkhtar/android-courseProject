0ac04de62cc3e8f030396b64c2581a487068ffa9
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2022-05-26 15:55:28 +0200

    Show logo of both teams instead of cricket pic

--- Cricket/app/src/main/java/com/ehb/cricket/adapters/MatchAdapter.kt
+++ Cricket/app/src/main/java/com/ehb/cricket/adapters/MatchAdapter.kt
@@ -11,8 +11,11 @@ import com.ehb.cricket.classes.Matches
 
 class MatchAdapter : RecyclerView.Adapter<MatchAdapter.ViewHolder>() {
 
-   private var matchName = arrayOf("PAK vs IND", "PAK vs BAN","PAK vs AUS", "PAK vs ENG","PAK vs SA","PAK vs IND", "PAK vs BAN","PAK vs AUS", "PAK vs ENG","PAK vs SA")
+   private var matchName = arrayOf("PAK vs IND", "PAK vs BAN","AUS vs PAK", "PAK vs ENG","WI vs PAK","PAK vs IND", "PAK vs BAN","AUS vs PAK", "PAK vs ENG","WI vs PAK")
+   private var imgTeam1 = arrayOf(R.drawable.pakistan, R.drawable.pakistan, R.drawable.australia, R.drawable.pakistan,R.drawable.west_indies,R.drawable.pakistan, R.drawable.pakistan, R.drawable.australia, R.drawable.pakistan,R.drawable.west_indies)
+   private var imgTeam2 = arrayOf(R.drawable.india, R.drawable.bangladesh, R.drawable.pakistan, R.drawable.england,R.drawable.pakistan,R.drawable.india, R.drawable.bangladesh, R.drawable.pakistan, R.drawable.england,R.drawable.pakistan)
    private var matchDateAndTime = arrayOf("28-05-2022, 5PM", "28-05-2022, 5PM","28-05-2022, 5PM", "28-05-2022, 5PM","28-05-2022, 5PM","28-05-2022, 5PM", "28-05-2022, 5PM","28-05-2022, 5PM", "28-05-2022, 5PM","28-05-2022, 5PM")
+
    // lateinit var matchName: List<Matches>
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MatchAdapter.ViewHolder {
@@ -30,17 +33,20 @@ class MatchAdapter : RecyclerView.Adapter<MatchAdapter.ViewHolder>() {
         // holder.name.text = matchName[position].name
         holder.name.text = matchName[position]
         holder.dateAndTime.text = matchDateAndTime[position]
-        holder.img.setImageResource(R.drawable.cricket)
+        holder.img1.setImageResource(imgTeam1[position])
+        holder.img2.setImageResource(imgTeam2[position])
     }
 
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        var img : ImageView
+        var img1 : ImageView
+        var img2 : ImageView
         var name : TextView
         var dateAndTime : TextView
 
         init {
-            img = itemView.findViewById(R.id.iv_img)
+            img1 = itemView.findViewById(R.id.iv_img)
+            img2 = itemView.findViewById(R.id.iv_img2)
             name = itemView.findViewById(R.id.tv_name)
             dateAndTime = itemView.findViewById(R.id.tv_info)
         }
