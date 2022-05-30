b9bb6248400954585e361305592ff3cc02b3e90b
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2022-05-30 12:49:17 +0200

    Nav in fav page

--- Cricket/app/src/main/java/com/ehb/cricket/Favourites.kt
+++ Cricket/app/src/main/java/com/ehb/cricket/Favourites.kt
@@ -1,11 +1,33 @@
 package com.ehb.cricket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.ehb.cricket.classes.Countries
+import com.ehb.cricket.classes.Matches
+import com.ehb.cricket.classes.Players
 
 class Favourites : AppCompatActivity() {
+
+    val matchList = mutableListOf<Matches>()
+    val playerList = mutableListOf<Players>()
+    val countryList = mutableListOf<Countries>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favourites)
+
+        val favIcon = findViewById<ImageView>(R.id.FavIcon)
+        favIcon.setOnClickListener {
+            val intent = Intent(this, Favourites::class.java)
+            startActivity(intent)
+        }
+
+        val backIcon = findViewById<ImageView>(R.id.backIcon)
+        backIcon.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
